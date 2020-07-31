35fc3e7bd738a4fe6ee58b3c426d7d43e7fa1543
Vitalii Bandyl <vitaliibandyl@gmail.com>
2020-07-31 19:02:15 +0300

    Database configured

---
+++ api/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Task(models.Model):
+    """Task model"""
+    TASK_STATUS = (
+        ('open', 'Open'),
+        ('in_process', 'In process'),
+        ('resolved', 'Resolved'),
+    )
+    TASK_PRIORITY = (
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+        ('now', 'Now'),
+    )
+    title = models.CharField(verbose_name="Title", max_length=200)
+    created = models.DateTimeField(verbose_name="Creation date", auto_now_add=True)
+    due_date = models.DateField(verbose_name="Due date")
+    status = models.CharField(verbose_name="Task status", max_length=10, choices=TASK_STATUS, default="Open")
+    priority = models.CharField(verbose_name="Task priority", max_length=6, choices=TASK_PRIORITY)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Task"
+        ordering = ['-due_date']
